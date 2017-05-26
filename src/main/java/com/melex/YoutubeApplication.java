78f35fddaf2bc9d822400dae538176331479f7a8
Melmo <melodymoselle@gmail.com>
2017-05-26 14:12:04 -0700

    Project setup

---
+++ src/main/java/com/melex/YoutubeApplication.java
@@ -0,0 +1,12 @@
+package com.melex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeApplication.class, args);
+	}
+}
